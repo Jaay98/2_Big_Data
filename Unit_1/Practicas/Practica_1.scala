032d2687925a0ebf17b5ed364cfdde625062bdfd
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2020-11-05 18:49:47 -0800

    Update Practica_1.scala

--- Unit_1/Practicas/Practica_1.scala
+++ Unit_1/Practicas/Practica_1.scala
@@ -1,6 +1,6 @@
 //Practice 1 
-//Author Alvarez Yanez Jose Alonso
-//17210526
+//Author Alvarez Yanez Jose Alonso,Quiroz Montes Yim Yetzhael
+//17210526,17210623
 
 
 //1.Calcular el radio  de un circulo
