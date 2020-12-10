0c970925e3257d5f500f94bb80b3181c9fb66c84
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2020-12-09 22:14:39 -0800

    Update Practice_8.scala

--- Unit_2/Practice_8.scala
+++ Unit_2/Practice_8.scala
@@ -8,3 +8,17 @@ Authors:
 -Big Data
 
 */
+
+//1. Import libraries and package
+package org.apache.spark.examples.ml
+import org.apache.spark.ml.classification.LinearSVC
+// Import a Spark Session. 
+import org.apache.spark.sql.SparkSession
+//Load the data stored in LIBSVM format as a DataFrame.
+val spark = SparkSession.builder.appName("LinearSVCExample").getOrCreate()
+
+val training = spark.read.format("libsvm").load("/usr/local/spark-2.3.4-bin-hadoop2.6/data/mllib/sample_libsvm_data.txt")
+val lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1)
+ val lsvcModel = lsvc.fit(training)
+ //print coefficients intercept
+println(s"Coefficients: ${lsvcModel.coefficients} Intercept: ${lsvcModel.intercept}")
