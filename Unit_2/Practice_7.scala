5928010e1f534d3d62a3b12d95e637f0a44c5c8d
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2020-12-09 21:54:17 -0800

    Practica7

--- Unit_2/Practice_7.scala
+++ Unit_2/Practice_7.scala
@@ -8,3 +8,63 @@ Authors:
 -Big Data
 
 */
+
+// Import MultilayerPerceptronClassifier y MulticlassClassificationEvaluator
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+// Import spark
+import org.apache.spark.sql.SparkSession
+
+/**
+ * An example for Multilayer Perceptron Classification.
+ */
+
+ // Create the object  MultilayerPerceptronClassifier
+object MultilayerPerceptronClassifierExample {
+
+// Define the function  main as parameter  
+  def main(): Unit = {
+    // Define the object spark=sparksession 
+    // MultilayerPerceptronClassifierExample
+    val spark = SparkSession
+      .builder
+      .appName("MultilayerPerceptronClassifierExample")
+      .getOrCreate()
+
+    // $example on$
+    //the data is loaded into the dataframe
+    val data = spark.read.format("libsvm")
+      .load("sample_multiclass_classification_data.txt")
+
+    // Data is divided into training and testing
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+    val train = splits(0)
+    val test = splits(1)
+
+    // The layers of the neural network are specified: in an array of size [4,5,4,3]
+    val layers = Array[Int](4, 5, 4, 3)
+
+    // Define the parameters of training
+    val trainer = new MultilayerPerceptronClassifier()
+      .setLayers(layers)
+      .setBlockSize(128)
+      .setSeed(1234L)
+      .setMaxIter(100)
+
+    // The model is trained
+    val model = trainer.fit(train)
+
+    // Calculate the precision of the dates of test
+    val result = model.transform(test)
+    val predictionAndLabels = result.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+
+    // Print pattern accuracy
+    println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+    // $example off$
+
+    spark.stop()
+  }
+}
