2464a10579539004f4dc41e6b750d79ccccfa758
yimquiroz <60414225+yimquiroz@users.noreply.github.com>
2020-12-10 22:18:48 -0800

    up

--- Unit_2/Practices/Practice_6.scala
+++ Unit_2/Practices/Practice_6.scala
@@ -1,9 +1,9 @@
 /*
-
-Practice 1
-
 Practice 6
+<<<<<<< HEAD
 
+=======
+>>>>>>> c53c5dd9299e4d21f607a1e9e381566bf6eeaaed
 
 Authors:
 -Alvarez Yanez Jose Aloso 
@@ -18,43 +18,50 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
-// Here the file is loaded and analyzed, turning it into a dataframe "sample_libsvm_data.txt"
+
+// 2- Here the file is loaded and analyzed, turning it into a dataframe "sample_libsvm_data.txt"
 val data = spark.read.format("libsvm").load("sample_libsvm_data.txt")
 
-//We adjust the entire data set to include the labels in the index
+// 3- We adjust the entire data set to include the labels in the index
 val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
-//we add our val = new vector followed by maxCategories with> 4
+
+// 4- we add our val = new vector followed by maxCategories with> 4
 val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
 
-// We divide the data in 2 to be able to do the tes
+// 5- We divide the data in 2 to be able to do the tes
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
-//we get the max number of iterations
+// 6- we get the max number of iterations
 val gbt = new GBTClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setMaxIter(10).setFeatureSubsetStrategy("auto")
 
-//Convert indexed tags to original tags
+// 7- Convert indexed tags to original tags
 val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
 
-//Label Converter
+// 8- Label Converter
 val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, gbt, labelConverter))
 
-// Runs the indexers.
+// 9- Runs the indexers.
 val model = pipeline.fit(trainingData)
 
-// Make predictions.
+// 10- Make predictions.
 val predictions = model.transform(testData)
 
-// Select example rows to display.
+// 11- Select example rows to display.
 //we can visualize the rows
 predictions.select("predictedLabel", "label", "features").show(20)
 
-// Select (prediction, true label) and compute test error.
+// 12- Select (prediction, true label) and compute test error.
 val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
 val accuracy = evaluator.evaluate(predictions)
-//Print accuracy
+
+// 13- Print accuracy
 println(s"Test Error = ${1.0 - accuracy}")
 
 val gbtModel = model.stages(2).asInstanceOf[GBTClassificationModel]
-//Print Learned Classification Model
-println(s"Learned classification GBT model:\n ${gbtModel.toDebugString}")
 
+// 14- Print Learned Classification Model
+println(s"Learned classification GBT model:\n ${gbtModel.toDebugString}")
+<<<<<<< HEAD
+
+=======
+>>>>>>> c53c5dd9299e4d21f607a1e9e381566bf6eeaaed
--- Unit_2/Practices/Practice_9.scala
+++ Unit_2/Practices/Practice_9.scala
@@ -17,7 +17,7 @@ def main(): Unit = {
      //Define the object spark=sparksession - MultilayerPerceptronClassifierEvaluator
    val spark = SparkSession.builder.appName("MulticlassClassificationEvaluator").getOrCreate()
    ////the data is loaded into the dataframe
-val inputData = spark.read.format("libsvm")load("data/mllib/sample_multiclass_classification_data.txt")
+val inputData = spark.read.format("libsvm")load("sample_multiclass_classification_data.txt")
 val Array(train, test) = inputData.randomSplit(Array(0.8, 0.2))
 val classifier = new LogisticRegression()
 .setMaxIter(10)
@@ -33,4 +33,9 @@ val predictions = ovrModel.transform(test)
 val evaluator = new MulticlassClassificationEvaluator()
 .setMetricName("accuracy")
 val accuracy = evaluator.evaluate(predictions)
+<<<<<<< HEAD
 println(s"Test Error = ${1 - accuracy}")}
+=======
+println(s"Test Error = ${1 - accuracy}")
+}
+>>>>>>> c53c5dd9299e4d21f607a1e9e381566bf6eeaaed
