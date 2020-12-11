65fd47f031c2a8d37840f7a3b8d3458550ac67b4
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2020-12-10 23:04:48 -0800

    update

---
+++ Unit_2/Evaluations/Evaluacion_2.scala
@@ -0,0 +1,26 @@
+/*
+Evaluacion Unidad_2
+
+Authors:
+-Alvarez Yanez Jose Alonso 
+-Quiroz Montes Yim Yetzhael
+
+-Big Data
+
+
+Desarrollar las siguientes instrucciones en Spark con el leguaje de programación Scala, 
+utilizando solo la documentacion de la librería de Machine Learning  Mllib de Spark y Google.
+    1. Cargar en un dataframe Iris.csv que se encuentra en https://github.com/jcromerohdz/iris, elaborar la liempieza de datos
+    necesaria para ser procesado por el siguiente algoritmo (Importante, esta limpieza debe ser por medio de un script de Scala en Spark) .
+       a. Utilice la librería Mllib de Spark el algoritmo de Machine Learning correspondiente a multilayer perceptron
+    2. ¿Cuáles son los nombres de las columnas?
+    3. ¿Cómo es el esquema?
+    4. Imprime las primeras 5 columnas.
+    5. Usa el metodo describe () para aprender mas sobre los datos del  DataFrame.
+    6. Haga la transformación pertinente para los datos categoricos los cuales seran nuestras etiquetas a clasificar.
+    7. Construya el modelos de clasificación y explique su arquitectura.
+    8. Imprima los resultados del modelo
+
+
+*/
+
---
+++ Unit_2/Practices/documentation.md
@@ -0,0 +1,20 @@
+##<p align="center"></p> INSTITUTO TECNOLOGICO DE TIJUANA </p>
+
+## Datos Masivos
+## Unidad 2
+
+
+
+
+
+
+#### Quiroz Montes Yim Yetzhael
+#### Alvarez Yanez Jose Alonso
+
+
+
+#### Practice 1
+
+
+
+
